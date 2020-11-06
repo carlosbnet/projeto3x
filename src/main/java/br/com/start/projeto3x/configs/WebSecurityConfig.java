7e424182eef272b27ceb6a80dcc5c956aa8cef05
Carlos <vandson.tec@gmail.com>
2020-11-06 20:38:54 -0300

    Resolvendo Cors

--- src/main/java/br/com/start/projeto3x/configs/WebSecurityConfig.java
+++ src/main/java/br/com/start/projeto3x/configs/WebSecurityConfig.java
@@ -10,6 +10,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.cors().and().csrf().disable();
     }
 }
